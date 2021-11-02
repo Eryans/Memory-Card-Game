6a2429f7d3f31b4e6021f644cf45a107b1119ecd
Julio <julesnoir@hotmail.fr>
2021-11-02 16:07:28 +0100

    Refactoring code and working on animation

--- js/card.js
+++ js/card.js
@@ -27,7 +27,7 @@ class Card{
         }
         this.isReturned = false;
         this.cardBody.classList.add("cardClass");
-        this.event = x => setTimeout(() => this.flip(x),0);
+        this.event = x => this.flip(x);
         this.cardBody.addEventListener("click",this.event);
         document.querySelector("main").appendChild(this.cardBody);
     }
@@ -35,68 +35,72 @@ class Card{
         if (this.canFlip){
             this.isReturned = true;
             this.canFlip = false;
+            returnedCards.push(this);
             checkCard(); 
             // Call CheckCard when selecting card, if we found a pair card are not clickable anymore so we do not need to check them
         }
         this.faceToShow();
     }
     faceToShow(){
-        (this.isReturned) ? this.cardBody.style.backgroundColor = this.frontColor : 
-        this.cardBody.style.backgroundColor = this.backColor;
+        (this.isReturned) ? (this.cardBody.style.backgroundColor = this.frontColor,
+        this.cardBody.style.transform = "rotate3d(0,1,0,180deg)") : 
+        (this.cardBody.style.backgroundColor = this.backColor,
+         this.cardBody.style.transform = "rotate3d(0,1,0,0deg)");
     }
 }
 
 // Game loop
 const CARDS = [];
+let returnedCards = [];
 init();
 
 // Functions
 // Creating CARDS and init game
 
 function init(){
-    const ID = []
+    const ID = [];
     for (let i = 0; i < 12; i++){
         randomId(ID);
-        CARDS.push(new Card(ID[i]));
+        // Fill an array with the randomised id
+        CARDS.push(new Card(ID[i])); 
     }
 }
-// Fill an array with the randomised id
 function randomId(array){
     let number = Math.ceil(Math.random()*6); // Use math.ceil to avoid returning 0
     array.filter(x => x === number).length < 2 ? array.push(number) : randomId(array);
 }
 
 function checkCard(){
-    let returnedCardArray = CARDS.filter(x => x.isReturned); 
-    if (returnedCardArray.length === 2){
+    if (returnedCards.length === 2){
         CARDS.forEach(x => x.canFlip = false);
-        if (returnedCardArray[0].id === returnedCardArray[1].id){
+        if (returnedCards[0].id === returnedCards[1].id){
             setTimeout(function(){
                 console.log("pair found");
-                returnedCardArray.forEach(x => x.isFound = true);
+                returnedCards.forEach(x => (x.isFound = true,
+                                            x.cardBody.style.opacity =  0.65));
                 CARDS.forEach(x => x.isFound ? x.canFlip = false : x.canFlip = true);
-                CARDS.splice(CARDS.indexOf(returnedCardArray[0]),1);
-                CARDS.splice(CARDS.indexOf(returnedCardArray[1]),1);
-                returnedCardArray = [];
+                CARDS.splice(CARDS.indexOf(returnedCards[0]),1);
+                CARDS.splice(CARDS.indexOf(returnedCards[1]),1);
+                returnedCards = [];
+                checkIfWin();
             },2000);
         }
         else {
             setTimeout(function(){
                 console.log("too bad");
-                returnedCardArray.forEach(x => (
+                returnedCards.forEach(x => (
                     x.isReturned = false,
                     x.faceToShow(),
                     x.canFlip = true,
-                    returnedCardArray = [],
+                    returnedCards = [],
                     CARDS.forEach(x => x.isFound ? x.canFlip = false : x.canFlip = true)
                 ))
             },2000);
         }
     }
-    checkIfWin();
     console.log(CARDS.length);
 }
 
 function checkIfWin(){
-    CARDS.length > 2 ? console.log("Game is still going") : console.log("You won !");
+    CARDS.length > 0 ? console.log("Game is still going") : console.log("You won !");
 }
