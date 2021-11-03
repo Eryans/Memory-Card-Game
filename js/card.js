468eff3ad7e5414e1ecbbfe15c9a5088b9dcc86a
Julio <julesnoir@hotmail.fr>
2021-11-03 14:58:34 +0100

    Game is playable and replayable !

--- js/card.js
+++ js/card.js
@@ -3,14 +3,14 @@ class Card{
         this.cardBody = this.createCard(id,colors,backImg);
         this.returnedCards = returnedCards;
         this.cards = cards;
-        this.canFlip = true;
+        this.canFlip = false;
         this.isFound = false;
         this.id = id;
         this.isReturned = false;
         this.cardBody.classList.add("cardClass");
         this.event = x => this.flip(x);
         this.cardBody.addEventListener("click",this.event);
-        this.faceToShow();
+        setTimeout(() => (this.faceToShow(),this.canFlip = true),2000)
         document.querySelector("main").appendChild(this.cardBody);
         console.log(this);
     }
@@ -43,6 +43,10 @@ class Card{
     }
 }
 class BoardGame{
+    static life = 5;
+    static getLife() { return this.life; }
+    static loseLife() { this.life = this.getLife() -1; }
+    static setLife(value) { this.life = value; }
     static checkCard(cards,returnedCards){
         if (returnedCards.length === 2){
             cards.forEach(x => x.canFlip = false);
@@ -68,24 +72,86 @@ class BoardGame{
                         cards.forEach(x => x.isFound ? x.canFlip = false : x.canFlip = true)
                     ));
                     returnedCards.length = 0;
+                    BoardGame.loseLife();
+                    LIFE.innerText = BoardGame.getLife();
+                    BoardGame.checkGameOver();
                 },2000);
             }
         }
     }
     static checkIfWin(cards){
-        cards.length > 0 ? console.log("Game is still going") : console.log("You won !");
+        if (cards.length === 0){
+            OptionBox.setMessage("You won !")
+            OptionBox.box();
+        }
+    }
+    static checkGameOver(){
+        if (BoardGame.getLife() === 0) {
+            OptionBox.setMessage("Game Over ! Play Again ?");
+            OptionBox.box();
+        }
     }
 }
+class OptionBox{
+    static message = "";
+    static box(){
+        // Blocker
+        let body = document.querySelector("body");
+        let block = document.createElement("div");
+        block.classList.add("blocker");
+        body.appendChild(block);
+        // Menu box
+        let ui = document.createElement("section");
+        ui.classList.add("uiClass");
+        ui.id = "menu";
+        ui.classList.add("uiClass","p-2","text-center","d-flex","flex-column","justify-content-center","gap-3");
+        ui.innerHTML = 
+        `<h2>Jules' Memory Game</h2>
+        <p>${this.message}</p>
+        <form>
+          <label for="theme">
+            Choose a theme
+            <select name="theme" id="theme">
+              <option value="Black">Black Card</option>
+              <option value="White">White Card</option>
+            </select>
+          </label>
+        </form>
+        <button id="playButton" class="btn btn-light">
+            Play
+        </button>`;
+        body.appendChild(ui);
+        
+        document.querySelector("#theme").addEventListener("change",function(x){
+            theme =  document.querySelector("#theme").value;
+        });
+
+        const playButton = document.querySelector("#playButton");
+        playButton.addEventListener("click",function(){
+            ui.remove();
+            block.remove();
+            init();
+        })
+    }
+    static setMessage(message){
+        this.message = message;
+    }
+}
+// Global
+const LIFE = document.querySelector("#Attempt");
+let theme = "Black";
 // Game loop
-init();
+OptionBox.box();
 // Functions
 function init(){
+    document.querySelector("main").innerHTML = ""; // Empty Main HTML to remove previous card if they exist
+    BoardGame.setLife(5);
+    LIFE.innerText = BoardGame.life;
     fetch("json/theme.json").then(function(response){
         return response.json()
     }).then(function(response){
         const ID = [];
         const CARDS = [];
-        let theme = "White";
         const COLORS = response[theme].frontCards;
         const BACKIMG = response[theme].backCard;
         console.log(COLORS);
